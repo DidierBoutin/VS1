bc8102b4d9ad3c8bf50e25d75297a94e890d7f81
DidierBoutin <didier.boutin1.pf1@gmail.com>
2018-06-07 11:31:36 +0200

    vs1

---
+++ src/app/menu.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Menu } from 'src/app/menu';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MenuService {
+
+  constructor() { }
+
+selectedMenu;
+
+
+//  menu: Menu[] = [
+// {
+//   'id' : 0,
+//   'name' : 'Bios',
+//   'description' : 'jjjlelm,p,,gp,ge,gepp,ge'
+// },
+// {
+//   'id' : 1,
+//   'name' : 'Contact',
+//   'description' : 'jjjkdlfdlvflk,b,jlelm,p,,gp,ge,gepp,ge'
+// },
+// {
+//   'id' : 2,
+//   'name' : 'Réalisation',
+//   'description' : 'jhgvvvvvvgepp,ge'
+// }
+//  ];
+// }
